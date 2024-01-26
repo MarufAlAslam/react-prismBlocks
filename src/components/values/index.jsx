aaa84aeef2b2176bd548e950a023b0d411ab2705
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-27 05:53:59 +0600

    values added

---
+++ src/components/values/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Values = () => {
+    const data = [
+        {
+            id: 1,
+            value: "2100+",
+            description: "Critical exploits detected"
+        },
+        {
+            id: 1,
+            value: "126M+",
+            description: "We secured on protocols"
+        },
+        {
+            id: 1,
+            value: "3B+",
+            description: "Total value monitored"
+        }
+    ]
+    return (
+        <div className='values py-[60px] bg-[#181818]'>
+            <div className="container">
+                <div className="flex justify-center items-center text-center lg:flex-row flex-col gap-[100px]">
+                    {
+                        data.map((item, index) => (
+                            <div key={item.id} className={`flex ${index !== 0 && "border-l border-[#282829]"} pl-[100px] justify-center text-center items-center`}>
+                                <div className="left">
+                                    <h2 className='font-semibold text-white text-[50px]'>
+                                        {item.value}
+                                    </h2>
+                                    <p>
+                                        {item.description}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Values
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Cards from '../../components/cards'
 import TrustedBy from '../../components/trusted-by'
+import Values from '../../components/values'
 
 const Homepage = () => {
     return (
         <>
             <Cards />
             <TrustedBy />
+            <Values />
         </>
     )
 }

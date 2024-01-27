095712cf07a04740cf393532f2629e7eb4d66675
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-27 07:30:10 +0600

    about page initiated

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/layout";
 import Homepage from "../pages/homepage";
+import AboutUs from "../pages/about-us";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Homepage />
+            },
+            {
+                path: "/about",
+                element: <AboutUs />
             }
         ]
     }
